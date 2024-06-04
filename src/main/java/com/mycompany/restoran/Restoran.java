e2d8ed28a706fc5f34d679d9dd90b51188a3a6c6
Walid Hanif Ataullah <walidhnf12@gmail.com>
2024-06-04 18:39:06 +0700

    first commit

---
+++ src/main/java/com/mycompany/restoran/Restoran.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restoran;
+
+/**
+ *
+ * @author walid
+ */
+public class Restoran {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
