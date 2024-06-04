946977347ed9dd415dd7e265e2b8c26b9d34837e
Walid Hanif Ataullah <walidhnf12@gmail.com>
2024-06-04 18:57:39 +0700

    Menambahkan class BahanBaku dan Menu

---
+++ src/main/java/com/mycompany/restoran/BahanBaku.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.restoran;
+
+/**
+ *
+ * @author walid
+ */
+
+public class BahanBaku {
+    private String namaBahan;
+    private int harga;
+
+    public BahanBaku(String namaBahan, int harga) {
+        this.namaBahan = namaBahan;
+        this.harga = harga;
+    }
+
+    public String getNamaBahan() {
+        return namaBahan;
+    }
+
+    public void setNamaBahan(String namaBahan) {
+        this.namaBahan = namaBahan;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
